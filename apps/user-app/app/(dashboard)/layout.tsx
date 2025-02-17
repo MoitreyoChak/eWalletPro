60f484bfb6e0f05f8fcaec9742b04ac3508da6d7
Moitreyo <cmoitreyo@gmail.com>
2025-02-17 13:03:00 +0530

    Added a new sidebar component-p2p

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -20,6 +20,7 @@ export default function Layout({
             icon={<TransactionsIcon />}
             title="Transactions"
           />
+          <SidebarItem href={"/p2p"} icon={<P2PIcon />} title="p2p" />
         </div>
       </div>
       {children}
@@ -79,6 +80,25 @@ function TransactionsIcon() {
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+      />
+    </svg>
+  );
+}
+
+function P2PIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
       />
     </svg>
   );
